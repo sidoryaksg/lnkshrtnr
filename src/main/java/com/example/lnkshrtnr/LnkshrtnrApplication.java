7c172d83c591f3101aeeb7bb7c398222f0f51206
sidoryak <sidoryak@gmail.com>
2022-07-03 23:26:32 +0700

    Init project

---
+++ src/main/java/com/example/lnkshrtnr/LnkshrtnrApplication.java
@@ -0,0 +1,13 @@
+package com.example.lnkshrtnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnkshrtnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnkshrtnrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lnkshrtnr/LnkshrtnrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lnkshrtnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LnkshrtnrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
